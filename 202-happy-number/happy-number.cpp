d2fca067f4dd954004e5fa40acaae1c86ad38959
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-06-03 23:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (58.28%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    int happy_function(int n) {
+        int res = 0;
+        for (; n > 0; n /= 10) res += pow(n % 10, 2); 
+        return res;
+    }
+
     bool isHappy(int n) {
-        int count = 0;
-        while (count < 100000){
-            int num = 0 ;
-            while(n > 0){
-                num += (n%10)*(n%10);
-                n = n / 10;
-            }
-            n = num;
-            if (n == 1){
-                return true;
-            }
-            count++;
+        set<int> numbers{4,16,37,58,89,145,42,20};
+        while (n != 1) {
+            if (numbers.contains(n)) return false;
+            n = happy_function(n);
         }
-        return false;
+        return true;
     }
 };
