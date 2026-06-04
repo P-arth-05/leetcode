f2ac33578ddf3b53764441a52be857868a4e365e
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-06-04 17:55:05 +0530

    Time: 2 ms (96.46%) | Memory: 8.5 MB (99.07%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,48 +1,27 @@
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int totalWaviness(int num1, int num2) {
-        // Numbers under 100 have fewer than 3 digits, so they can't have a peak or valley
-        if (num2 <= 100) {
-            return 0;
+        int t = 0;
+        for (int i=num1; i <= num2; i++) {
+            t += wavy(i);
         }
-        if (num1 <= 100) {
-            num1 = 100;
-        }
-        
-        int count = 0;
-        
-        // Loop directly from num1 to num2. No need to store them in an array!
-        for (int i = num1; i <= num2; i++) {
-            count += checkWavy(i);
-        }
-        
-        return count;
+        return t;
     }
 
-    int checkWavy(int n) {
-        vector<int> num;
-        
-        // Extract digits
-        for (; n > 0; n /= 10) {
-            num.push_back(n % 10); // FIXED: push_back instead of pushback
+private:
+    int wavy(int n) {
+        if (n < 101) return 0;
+        int w = 0, a, b, c;
+
+        c = n%10, n /= 10;
+        b = n%10, n /= 10;
+    
+        while (n) {
+            a = n%10;
+            if ((b>a && b>c) || (b<a && b<c)) w++;
+            c=b, b=a, n /= 10;
         }
-        
-        int count = 0;
-        int size = num.size(); // Safe integer cast to prevent underflow
-        
-        // Check for local minima and maxima
-        for (int i = 1; i < size - 1; i++) {
-            if (num[i] < num[i - 1] && num[i] < num[i + 1]) {
-                count++;
-            }
-            else if (num[i] > num[i - 1] && num[i] > num[i + 1]) {
-                count++;
-            }
-        }
-        return count;
+
+        return w;
     }
 };
