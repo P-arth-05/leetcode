3dfac9aa74c73982e2a8699159422d2d8ee4eae8
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-06-23 21:53:58 +0530

    Time: 57 ms (55.23%) | Memory: 24 MB (25.71%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* p = headA;
+        ListNode* q = headB;
+
+        while (p != q) {
+            p = (p == nullptr) ? headB : p->next;
+            q = (q == nullptr) ? headA : q->next;
+        }
+
+        return p;
+    }
+};
