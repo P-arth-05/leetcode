6bd7de11fcd5ec85906c88495a4e1a3170edfe58
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-06-28 10:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (49.26%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        int cnt = 0;
+        while (n) {
+            cnt += (n & 1);
+            n >>= 1;
+        }
+        return cnt == 1;
+    }
+};
