dda379aa8c1b6115f7f655db4663c308a21c2139
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-07-01 23:28:09 +0530

    Time: 604 ms (19.86%) | Memory: 203.9 MB (33.42%) - LeetSync

---
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        queue<pair<int,int>> q;
+
+        // Multi-source BFS
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int dr[4] = {-1, 1, 0, 0};
+        int dc[4] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                    dist[nr][nc] == INT_MAX) {
+                    dist[nr][nc] = dist[r][c] + 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        auto can = [&](int safe) {
+            if (dist[0][0] < safe) return false;
+
+            vector<vector<bool>> vis(n, vector<bool>(n, false));
+            queue<pair<int,int>> q;
+
+            q.push({0,0});
+            vis[0][0] = true;
+
+            while (!q.empty()) {
+                auto [r,c] = q.front();
+                q.pop();
+
+                if (r == n-1 && c == n-1)
+                    return true;
+
+                for (int k = 0; k < 4; k++) {
+                    int nr = r + dr[k];
+                    int nc = c + dc[k];
+
+                    if (nr >= 0 && nr < n &&
+                        nc >= 0 && nc < n &&
+                        !vis[nr][nc] &&
+                        dist[nr][nc] >= safe) {
+
+                        vis[nr][nc] = true;
+                        q.push({nr,nc});
+                    }
+                }
+            }
+
+            return false;
+        };
+
+        int lo = 0, hi = 2 * n, ans = 0;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (can(mid)) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
