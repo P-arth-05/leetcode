3cab088d29ae5d12fb300cc51fa6477cb0e89688
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-07-04 22:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (9.59%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<pair<TreeNode*, TreeNode*>> q;
+        q.push({root->left, root->right});
+
+        while (!q.empty()) {
+            auto [left, right] = q.front();
+            q.pop();
+
+            if (!left && !right)
+                continue;
+
+            if (!left || !right)
+                return false;
+
+            if (left->val != right->val)
+                return false;
+
+            q.push({left->left, right->right});
+            q.push({left->right, right->left});
+        }
+
+        return true;
+    }
+};
