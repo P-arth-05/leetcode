9855dbf1de26800889c90ace1e0419a8c67665b1
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-07-20 22:52:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (41.49%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void dfs(TreeNode* root, int depth) {
+        if (!root) return;
+
+        if (depth == ans.size())
+            ans.push_back(root->val);
+
+        dfs(root->right, depth + 1);
+        dfs(root->left, depth + 1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root, 0);
+        return ans;
+    }
+};
