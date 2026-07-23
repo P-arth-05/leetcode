189a951d4bcfd47ecefcd1220d978d7ccf849319
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-07-23 23:11:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.33%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    void dfs(int start, int k, int target) {
+        if (target == 0 && k == 0) {
+            ans.push_back(curr);
+            return;
+        }
+
+        if (target < 0 || k == 0)
+            return;
+
+        for (int i = start; i <= 9; i++) {
+            curr.push_back(i);
+            dfs(i + 1, k - 1, target - i);
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        dfs(1, k, n);
+        return ans;
+    }
+};
