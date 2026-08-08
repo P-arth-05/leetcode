3ae51cc73e8cb48773ea8f43f09f43fe3959eeb0
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-08-08 23:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (99.26%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    pair<int, int> solve(TreeNode* root) {
+        if (root == NULL)
+            return {0, 0};
+
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+
+        // Rob current node
+        int rob = root->val + left.first + right.first;
+
+        // Don't rob current node
+        int notRob = max(left.first, left.second)
+                   + max(right.first, right.second);
+
+        return {notRob, rob};
+    }
+
+    int rob(TreeNode* root) {
+        auto ans = solve(root);
+
+        return max(ans.first, ans.second);
+    }
+};
