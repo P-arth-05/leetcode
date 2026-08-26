a602d5a7e7e6e70b5b8f3602af5901f79f4b5fb0
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2026-08-26 23:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (82.87%) - LeetSync

---
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+
+        int left = 0;
+        int ones = 0;
+
+        string ans = "";
+
+        for (int right = 0; right < n; right++) {
+
+            if (s[right] == '1')
+                ones++;
+
+            // Too many 1s
+            while (ones > k) {
+                if (s[left] == '1')
+                    ones--;
+
+                left++;
+            }
+
+            // Remove unnecessary leading zeros
+            while (ones == k && s[left] == '0') {
+                left++;
+            }
+
+            // Current window has exactly k ones
+            if (ones == k) {
+                string curr = s.substr(left, right - left + 1);
+
+                if (ans.empty() ||
+                    curr.size() < ans.size() ||
+                    (curr.size() == ans.size() && curr < ans)) {
+
+                    ans = curr;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
